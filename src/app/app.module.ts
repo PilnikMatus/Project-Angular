ab580de8e0cc3941262b7b9fce13db673b471dba
danstepanek <61269423+danstepanek@users.noreply.github.com>
2020-04-15 12:48:16 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { JobsComponent } from './jobs/jobs.component';
 import { AdminsComponent } from './admins/admins.component';
 import { LogsComponent } from './logs/logs.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
